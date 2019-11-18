dc203dbf52eadb7bff84ad97f743350dbfca1f2b
andikas95-dev <andikaperdana95@gmail.com>
2019-11-18 07:48:35 +0700

    fix seeder user

--- seeders/20191026014105-demo-users.js
+++ seeders/20191026014105-demo-users.js
@@ -4,10 +4,11 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('users', [
       {
-          username: 'admin',
-          password: 'admin'
+        username: 'admin',
+        password: 'admin',
+        userImg: 'https://images.unsplash.com/photo-1573997641075-a41a148b9af3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80'
       },
-  ], {});
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
